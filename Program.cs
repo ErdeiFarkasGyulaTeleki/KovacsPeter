d1282f176c37015b484e21b7ad3b6a017c923ebe
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-09 14:47:42 +0200

    Kód lemásolása

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Diak
+{
+    public string nev;
+    public int eletkor;
+    public List<int> jegyek;
+
+    public Diak(string n)
+    {
+        nev = n;
+    }
+
+    public void AtlagSzamolas()
+    {
+        int osszeg = 0;
+        foreach (var j in jegyek)
+        {
+            osszeg += j;
+        }
+        Console.WriteLine(nev + " átlaga: " + (osszeg / jegyek.Count));
+    }
+}
+
+class Program
+{
+    static void Main ()
+    {
+        Diak d = new Diak("Kovács Péter");
+        d.eletkor = -5;
+        d.jegyek.Add(5);
+        d.jegyek.Add(4);
+
+        d.AtlagSzamolas();
+    }
+}
